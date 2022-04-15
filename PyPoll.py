4a8e518c87f18b584b07fd2aa9402e865b977138
tbrech4 <tbrech4@gmail.com>
2022-04-15 16:30:41 -0500

    Updating files

---
+++ PyPoll.py
@@ -0,0 +1,24 @@
+import csv
+
+import os
+
+# Assign a variable for the file to load and the path.
+
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+
+
+outfile = open(file_to_save, "w")
+
+outfile.write("Hello World")
+
+outfile.close
